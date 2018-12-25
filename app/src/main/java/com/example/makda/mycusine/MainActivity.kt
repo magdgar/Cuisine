69fdd8e716f26e771afe0fcc4b03b722c8f8e805
Magdalena Gargas <magdgar@gmail.com>
2018-12-25 21:02:25 +0100

    Add onItemClick listener for list items

--- app/src/main/java/com/example/makda/mycusine/MainActivity.kt
+++ app/src/main/java/com/example/makda/mycusine/MainActivity.kt
@@ -3,17 +3,23 @@ package com.example.makda.mycusine
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.content.Intent
+import android.view.View
+import android.widget.AdapterView
 import android.widget.ListView
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), AdapterView.OnItemClickListener {
+
     private var adapter: RecipeAdapter?= null
     private val items: ArrayList<ListItem> = ArrayList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        listView.onItemClickListener = this
 
         val list = findViewById<ListView>(R.id.listView)
         items.add(ListItem(R.drawable.abc_btn_check_to_on_mtrl_015, "name", "desc"))
@@ -25,6 +31,10 @@ class MainActivity : AppCompatActivity() {
         floatingActionButton.setOnClickListener {
             addNewRecipe()
         }
+    }
+
+    override fun onItemClick(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        Toast.makeText(this, "Yey", Toast.LENGTH_SHORT).show()
     }
 
     private fun addNewRecipe() {
